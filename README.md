65a8b3541a40024da1567e606965ae0572de3653
ox-i <oxi1224@o2.pl>
2022-08-30 16:59:23 +0200

    initial

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# KD-Winrate-Display
+<h1> KeyDrop Winrate Display <h1>
+<h2> Jak zainstalować? <h2>
+<h3> Przeglądarki bazowane na chromium:<h3>
+<p>Są to np. Chrome, Opera GX, Microsoft Edge
+<ul>
+<li>Pobierz [ten](https://github.com/oxi1224/KD-Winrate-Display/archive/refs/heads/main.zip) plik</li>
+<li>Rozpakuj go do folderu</li>
+<li>Wejdź na stronę: chrome://extensions (działa na wyżej wspomnianych przeglądarkach)</li>
+<li>Włącz tryb programisty</li>
+<li>Kliknij przycisk "Załaduj rozpakowane"</li>
+<li>Wybierz folder z rozszerzeniem</li>
+</ul>
+</p>
+<h3>Inne przeglądarki:</h3>
+<p>Ze względu na to że w każdej przeglądarce może być inny sposób, proszę wyszukaj "jak zainstalować rozszerzenie z pliku na przeglądarce X"</p>
---
+++ index.js
@@ -0,0 +1,84 @@
+// event: div.case__name
+// unified: div.relative.grid.grid-cols-1.grid-rows-1.transition-all
+// ids: [
+//  section.youtubers-cases
+//  section.cs-go-kings
+//  section.cs-go-cage
+//  section.cs-go-magic
+//  section.cs-go-heroes
+//  section.cs-go-premium
+//  section.cs-go-skins
+//  section.cs-go-guns
+//  section.gold-area
+// ]
+//
+// NAME class: div.text-center.text-white.uppercase.rounded-lg
+window.onload = (async () => {
+  const goldenNames = ['foster', 'predator', 'optimal', 'solar', 'wind', 'ruby', 'shark', 'wrap', 'samurai', 'atum', 'gold-digger', 'rubin-rain', 'amethyst-light', 'diamond-sky', 'emerald-cut'];
+  const sectionIds = [
+    "limited-edition",
+    "youtubers-cases",
+    "cs-go-kings",
+    "cs-go-cage",
+    "cs-go-magic",
+    "cs-go-heroes",
+    "cs-go-premium",
+    "cs-go-skins",
+    "cs-go-guns",
+    "gold-area"
+  ];
+  const url = 'https://raw.githubusercontent.com/oxi1224/files/main/data-new.json';
+  
+  (async () => {
+    const data = await (await fetch(url)).json();
+    if (/https:\/\/key-drop.com\/(pl|en)\/skins\/category\/.+/.test(window.location.href)) {
+      const caseName = (window.location.href.split(/https:\/\/key-drop.com\/(pl|en)\/skins\/category\//))[2];
+      const caseData = data[caseName];
+      if (!caseData) return;
+      const profitElm = document.createElement('div');
+      const looseElm = document.createElement('div');
+      const statsElm = document.createElement('div');
+      document.body.style.position = 'relative';
+      profitElm.style.cssText = 'background-color: #5cd65c; border: 2px solid #2eb82e; position: fixed; top: 20px; left: 20px; font-size: 13px; color: white; text-align: center; width: fit-content; height: fit-content; z-index: 9999999; border-radius: 20px; padding: 1rem 1.5rem;';
+      profitElm.innerHTML = `Szanse na X razy zwortu:<br>${(Object.entries(caseData.profitChances).map(([k, v]) => `${k}: ${v}`)).join('<br>')}`;
+      looseElm.style.cssText = 'background-color: #ff3333; border: 2px solid #cc0000; position: fixed; top: 20px; right: 20px; font-size: 13px; color: white; text-align: center; width: fit-content; height: fit-content; z-index: 9999999; border-radius: 20px; padding: 1rem 1.5rem;';
+      looseElm.innerHTML = `Szanse na X razy straty:<br>${(Object.entries(caseData.looseChances).map(([k, v]) => `${k}: ${v}`)).join('<br>')}`;
+      statsElm.style.cssText = 'background-color: #3e321e; border: 1px solid #dcae64; position: fixed; top: 20px; left: 50%; transform: translateX(-50%); font-size: 13px; color: white; text-align: center; width: fit-content; height: fit-content; z-index: 99999; border-radius: 5px; padding: 1rem 1.5rem;';
+      statsElm.innerHTML = `Szansa na <b>wygraną</b>: ${caseData.winChance}%<br>Szansa na <b>przegraną</b>: ${caseData.looseChance}%`;
+      document.body.appendChild(profitElm);
+      document.body.appendChild(looseElm);
+      document.body.appendChild(statsElm);
+      console.log(statsElm);
+    }
+
+    if (window.location.href.match(/https:\/\/key-drop.com\/(pl|en)\//)) {
+      // eslint-disable-next-line no-constant-condition
+      if (!document.getElementById(sectionIds[0])) while (true) {
+        await new Promise(r => setTimeout(r, 1000));
+        if (document.getElementById(sectionIds[0])) break;
+      }
+      // css-vgih4x css-rvmde
+      const eventCases = [...document.querySelectorAll('div.case--event')];
+      const otherCases = (() => {
+        const returnArr = [];
+        sectionIds.forEach(sID => {
+          const s = document.getElementById(sID);
+          returnArr.push(...s.querySelectorAll('div.relative.grid.grid-cols-1.grid-rows-1.transition-all'));
+        });
+        return returnArr;
+      })();
+      const cases = eventCases.concat(otherCases);
+      cases.forEach(c => {
+        if (!c) return;
+        const caseName = (((c.querySelector('a.z-20.w-full.h-full.col-start-1.row-start-1.row-end-3') ?? c.querySelector('a.case__link'))?.href)?.split('/skins/category/'));
+        const caseData = data[caseName[1]];
+        if (!caseData) return;
+        c.style.position = 'relative';
+        const infoEl = document.createElement('div');
+        infoEl.style.cssText = `${(caseData?.winChance < 30 || caseData?.pricePerPln > 350) ? 'background-color: #ff3333; border: 2px solid #cc0000;' : 'background-color: #5cd65c; border: 2px solid #2eb82e;'} position: absolute; top: 50%; left: 50%; transform: translate(-50%, -50%); font-size: 13px; color: white; text-align: center; width: fit-content; height: fit-content; z-index: 9999999; border-radius: 20px; padding: .25rem .5rem;`;
+        infoEl.textContent = goldenNames.includes(caseName[1]) ? `Średni gold na zł: ${caseData.pricePerPln}` : `Winrate: ${caseData.winChance}%`;
+        c.appendChild(infoEl);
+      });
+    }
+  })();
+})();
